8b30b363586c8822801f3a348b964ead0e00965e
Owen Parkins <oparkins@gmail.com>
2019-12-29 12:30:31 -0700

    Added mapHashToPass and start of InitPass to rgpm

--- src/rgpm.js
+++ src/rgpm.js
@@ -29,8 +29,8 @@ class RGPM {
     }
 
     getStorage() {
-        let Storage =  StorageFactory.getLocalStorage();
-        let storageLocation = Storage.readFile("storageLocation");
+        const Storage =  StorageFactory.getLocalStorage();
+        const storageLocation = Storage.readFile("storageLocation");
         switch(storageLocation) {
             case null:
             case "localStorage":
@@ -38,6 +38,42 @@ class RGPM {
             default:
                 throw new NotImplementedError("Specific Storage Location Unknown: " + storageLocation);
         }
+    }
+
+    initPass(service_record, master_password) {
+        const master_key = Crypto.digest(master_password);
+        const record_concat = Crypto.null_concat(service_record.locator, service_record.identifier, service_record.revision);
+        const record_hashed = Crypto.digest(record_concat);
+
+        for(let i = 0; i < service_record.iter_t; i++) {
+            let record_hashed = Crypto.hmac(master_key, record_hashed);
+        }
+    }
+
+    combineAllCharacterSets(requirements) {
+        let characters = [];
+
+        for(const set_index in requirements.character_sets) {
+            characters = characters.concat(requirements.character_sets[set_index]["characters"]);
+        }
+
+        return characters;
+    }
+
+    mapHashToPass(hash, requirements) {
+        const character_set = this.combineAllCharacterSets(requirements);
+
+        let password = "";
+        let index = 0;
+        for(let i = 0; i < hash.length; i++) {
+            index = (index + hash[i]) % character_set.length;
+            password = password + character_set[index];
+        }
+        let last_index = 64;
+        if (requirements.max_password_length != null) {
+            last_index = Math.min(requirements.max_password_length, 64);
+        }
+        return password.substring(0, last_index);
     }
 }
 
--- test/rgpm.test.js
+++ test/rgpm.test.js
@@ -18,6 +18,12 @@ describe("RGPM", () => {
 
 describe("RGPM methods", () => {
     let rgpm = undefined;
+    const prml_1 = {"max_password_length": 23, "character_sets": [ { "name": "lowercase", "characters": ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]}, { "name": "uppercase", "characters": ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]}]};
+    const prml_2 = {"max_password_length": 7, "character_sets": [ { "name": "halfnhalf", "characters": ["a","b","c","d","e","f","g","h","i","j","k","l","m", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]}]};
+    const prml_3 = {"character_sets": [ { "name": "halfnhalf", "characters": ["a","b","c","d","e","f","g","h","i","j","k","l","m", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]}, {"name:": "numbers", "characters": ["1","2","3","4","5","6","7","8","9","0"]}]};
+
+
+
     beforeEach(() => {
         rgpmlib = require("../src/rgpm");
         rgpm = new rgpmlib();
@@ -46,5 +52,56 @@ describe("RGPM methods", () => {
             expect(rgpm.deleteRecord).toBeDefined();
         });
     });
+
+    describe("getStorage method", () => {
+        it("should exist", () => {
+            expect(rgpm.getStorage).toBeDefined();
+        });
+
+        it("should return local storage on first initialization", () => {
+            // Other tests have been used to verify that the correct
+            // class is return on each supported platform. This is
+            // just checking to ensure that our method of selecting 
+            // storage works properly.
+            expect(rgpm.getStorage().type()).toEqual("nodeStorage");
+        });
+    });
+
+    describe("combineAllCharacterSets method", () => {
+        it("should exist", () => {
+            expect(rgpm.combineAllCharacterSets).toBeDefined();
+        });
+
+        it.each([
+            [prml_1, ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]],
+            [prml_2, ["a","b","c","d","e","f","g","h","i","j","k","l","m", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]],
+            [prml_3, ["a","b","c","d","e","f","g","h","i","j","k","l","m", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1","2","3","4","5","6","7","8","9","0" ]]
+        ])("should produce the correct result", (prml, output) => {
+            expect(rgpm.combineAllCharacterSets(prml)).toEqual(output);
+        });
+    });
+
+    describe("mapHashToPass method", () => {
+        it("should exist", () => {
+            expect(rgpm.mapHashToPass).toBeDefined();
+        });
+
+        hash = [
+            244, 233,  27,  99, 123, 144,  55,  16,   1, 245,  51,
+             96, 184, 164, 228,  74, 175, 235,  26,  37, 103, 169,
+             46, 199, 165,   3, 148, 165,  24, 192, 174, 237,  29,
+             58,   0,  21,  64, 239, 245, 244, 216, 220, 242, 210,
+             21, 205, 248, 110, 112, 204, 205, 167, 218, 163, 160,
+            192, 244, 216, 182, 243, 214, 137, 152, 171
+        ];
+        it.each([
+            [hash, prml_1, "KjKFYMPfgRQIksMiBcCnmzt"],
+            [hash, prml_2, "kjkfYmP"],
+            [hash, prml_3, "3ja2ggZfg0Ocg1ce0Sije4dWhkO0X04Oh44Og4WOOSicXmikOc2OQ0P2TTVmkdlc"]
+        ])("should produce the correct result", (hash, prml, output) => {
+            expect(rgpm.mapHashToPass(hash, prml)).toEqual(output);
+        });
+    });
 });
+
 
